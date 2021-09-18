e435612a457630be0a44bc2dad59498c5b3d5793
devwojtek <devwojtek99@gmail.com>
2021-09-18 07:29:21 -0400

    add search to message controller

--- api/controllers/messages_api_controller.rb
+++ api/controllers/messages_api_controller.rb
@@ -3,6 +3,61 @@ controller :messages do
   description "This API allows you to access message details"
   authenticator :server
 
+  action :search do
+    title "Search messages"
+    description "Search messages"
+    # param :status, "The status of messages", :type => [String, Array], :required => false
+    param :search, "The search filter", :type => String, :required => false
+    param :from, "The from email", :type => String, :required => false
+    param :to, "The recipient email", :type => String, :required => false
+    param :before, "The before filter", :type => String, :required => false
+    param :after, "The after filter", :type => String, :required => false
+    param :order, "The order direction", :type => String, :required => false
+    param :orderby, "The order by", :type => String, :required => false
+    param :limit, "The limit of the messages", :type => Integer, :required => false
+    param :offset, "The offset of the messages", :type => Integer, :required => false
+    returns Array, :structure => :message, :structure_opts => {:paramable => {:expansions => false}}
+
+    action do
+      begin
+        where = Hash.new
+        where[:status] = params.status if params.status.present?
+        where[:mail_from] = params.from if params.from.present?
+        where[:rcpt_to] = params.to if params.to.present?
+        where[:subject] = { :like => params.search } if params.search.present?
+
+        time_range = {}
+        if params.before.present?
+          time_range = time_range.merge(:less_than_or_equal_to => params.before.to_datetime.to_i)
+        end
+        if params.after.present?
+          time_range = time_range.merge(:greater_than_or_equal_to => params.after.to_datetime.to_i)
+        end
+        where[:timestamp] = time_range
+
+        messages = identity.server.message_db.select(
+          'messages',
+          {
+            limit: params.limit || 20,
+            offset: params.offset || 0,
+            order: params.orderby || 'id',
+            direction: params.order || 'ASC',
+            where: where
+          }
+        )
+
+
+        messages.map do |d|
+          obj = identity.server.message_db.new_message(d)
+          structure :message, obj
+        end
+        # {messages: messages}
+      rescue Postal::MessageDB::Message::NotFound => e
+        error "MessageNotFound #{e}"
+      end
+    end
+  end
+
   action :message do
     title "Return message details"
     description "Returns all details about a message"
